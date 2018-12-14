ebd1ecf0429e180c9a8307fffe27ee4d8ab4ac37
coolbrobikash <44962712+coolbrobikash@users.noreply.github.com>
2018-12-14 15:59:21 +0900

    test

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello World</h1>
+      <h2>Welcome to ReactJS Example</h2>
     </div>
   );
 }
